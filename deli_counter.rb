fb48e8ed06a3546df68f1e9d62b235c931770130
Calie Rushton <calielrushton@gmail.com>
2019-01-02 15:41:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   else # insert the position number in front of each person
     queue = []
     katz_deli.each_with_index { |person, index| queue << "#{index + 1}. #{person}" }
-    
+
     puts "The line is currently: #{queue.join(", ")} "
   end
 end

6b1503e736f23cba736ef622bdb0cd3800da7fba
Calie Rushton <calielrushton@gmail.com>
2019-01-02 15:38:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else
-    puts "The line is currently: #{katz_deli}"  
+  else # insert the position number in front of each person
+    queue = []
+    katz_deli.each_with_index { |person, index| queue << "#{index + 1}. #{person}"}
+    puts "The line is currently: " + result array
   end
 end
